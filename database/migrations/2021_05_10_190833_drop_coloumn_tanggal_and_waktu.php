c27a5aef758a3502478255caef4786e41ca438d4
Frumentius Daneswara <frumentius44@gmail.com>
2021-05-16 20:44:13 +0700

    drop some coloumns with migrations

--- database/migrations/2021_05_10_190833_drop_coloumn_tanggal_and_waktu.php
+++ database/migrations/2021_05_10_190833_drop_coloumn_tanggal_and_waktu.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddFieldStatusReservasi extends Migration
+class DropColoumnTanggalAndWaktu extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class AddFieldStatusReservasi extends Migration
      */
     public function up()
     {
-        Schema::table('reservasi', function (Blueprint $table) {
-            $table->string('status_reservasi');
+        Schema::table('stok_keluar', function (Blueprint $table) {
+            $table->dropColumn('tanggal');
+            $table->dropColumn('waktu');
         });
     }
 
@@ -25,8 +26,9 @@ class AddFieldStatusReservasi extends Migration
      */
     public function down()
     {
-        Schema::table('reservasi', function (Blueprint $table) {
-            //
+        Schema::table('stok_keluar', function (Blueprint $table) {
+            $table->date('tanggal');
+            $table->time('waktu');
         });
     }
 }
